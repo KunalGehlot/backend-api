65a2e925fc2dddb3f05c8058c465f6bebbf7325f
Kunal Gehlot <kunal.g@360hvpl.com>
2022-08-27 17:13:36 +0530

    added exception handling in requests

--- utils/yt_api.py
+++ utils/yt_api.py
@@ -1,4 +1,7 @@
+import logging
 import requests
+
+logger = logging.getLogger(__name__)
 
 BASE_URL = "https://www.googleapis.com/youtube/v3/search"
 HEADERS = {
@@ -27,6 +30,18 @@ async def list_videos(
         "publishedAfter": published_after,
     }
 
-    res = await requests.get(BASE_URL, headers=HEADERS, params=params)
-
-    return res
+    try:
+        res = await requests.get(BASE_URL, headers=HEADERS, params=params)
+        res.raise_for_status()
+        return res
+    except requests.exceptions.HTTPError as errh:
+        logger.error("Http Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        logger.error("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        logger.error("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        if "403" in str(err):
+            logger.error("Quota exceeded")
+            return 403
+        logger.error("OOps: Something Else", err)
